ff92d1d2385ed443406ee52fba753c533ad98cce
CodingWithLuis <codewithluis11@gmail.com>
2021-12-08 12:07:17 -0600

    Preparing Product Model to Import Data

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->numberBetween(10, 200),
+            'quantity_left' => $this->faker->numberBetween(10, 30),
+            'category_id' => rand(1, 25),
+        ];
+    }
+}
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Productos
+                    <a href="#" class="btn btn-danger float-right">Importar Productos</a>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    <table class="table table-bordered table-striped products_table" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th scope="col">Producto</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Cantidad en inventario</th>
+                                <th scope="col">Categoria</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('scripts')
+<script>
+    $(document).ready(function() {
+
+        $('.products_table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{route('products.index')}}",
+            responsive: true,
+            columns: [{
+                    data: 'name',
+                    name: 'products.name'
+                },
+                {
+                    data: 'description',
+                    name: 'description'
+                },
+                {
+                    data: 'price',
+                    name: 'price',
+                    searchable: false
+                },
+                {
+                    data: 'quantity_left',
+                    name: 'quantity_left',
+                    searchable: false
+                },
+                {
+                    data: 'category.name',
+                    name: 'category.name'
+                }
+            ]
+        })
+    })
+</script>
+@endsection
